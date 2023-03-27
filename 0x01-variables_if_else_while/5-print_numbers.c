37343cf13e7b61c759a4357ab1a5cc5cbc221bc6
Kobby <95kobby@gmail.com>
2023-03-27 10:06:56 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - if else statements
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
