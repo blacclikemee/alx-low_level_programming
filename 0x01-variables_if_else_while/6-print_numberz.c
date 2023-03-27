2cab905167bf004defd092889e5c62e217f0e51c
Kobby <95kobby@gmail.com>
2023-03-27 10:07:53 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - if else statements
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}
