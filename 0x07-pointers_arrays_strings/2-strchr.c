2c0be8a632aefd81cba205133f788ada291a9470
Kobby <95kobby@gmail.com>
2023-04-07 20:11:26 -0700

    Task 02

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
- * _strchr - Function that looks for a particular value in a string
- * and return a pointer to it. Else, returna a null value
- * @s: Memory area to be searched for a specific value
- * @c: character to be searched for
- *
- * Return: s(Pointer to the first valueof c) or null(if c is not found)
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+	int i = 0;
 
-	return (NULL);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
+
