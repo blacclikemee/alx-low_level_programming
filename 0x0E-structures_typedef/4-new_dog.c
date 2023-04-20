c1d3f37d4ded20f8e7f66b2fa79a2f271fca4955
Kobby <95kobby@gmail.com>
2023-04-19 19:25:19 -0700

    Task 03

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * new_dog - Function that creates a new dog
+ * @name: Name of new dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
