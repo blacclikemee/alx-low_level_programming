4d403019bf6dcd60b5c930c872e0caffbae73b40
Kobby <95kobby@gmail.com>
2023-04-19 20:21:04 -0700

    Final Task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+  * main - Main function in code
+  * @argc: rgumnet count
+  * @argv: Argument vectpr
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
