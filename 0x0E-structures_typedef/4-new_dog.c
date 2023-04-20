4245b50bac87c1839722d738efd798e6a36d66cf
Kobby <95kobby@gmail.com>
2023-04-19 19:32:14 -0700

    Task 03 redone

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,89 @@
+#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * new_dog - Function that creates a new dog
- * @name: Name of new dog
- * @age: Age of dog
- * @owner: Owner of dog
+ * len - findlength of string
+ * @str: string
+ * Return: length
+ */
+
+int len(char *str)
+{
+	int i;
+
+	for (i = 0;  *(str + i); i++)
+		;
+
+	return (i);
+}
+
+/**
+ * strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+
+char *strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= len(src); i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/**
+ * new_dog - create new instance of struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Return:  initialised instance of struct dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog1;
+	char *copy_of_name;
+	char *copy_of_owner;
 
+	dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
+	if (dog1 == NULL)
+		return (NULL);
+
+	dog1->age = age;
+	/* make copies of struct members and validate, else free on error */
+	/* set values of struct members to copies of arguments or set to NULL */
+	if (name != NULL)
+	{
+		copy_of_name = malloc(len(name) + 1);
+
+		if (copy_of_name == NULL)
+		{
+			free(dog1);
+			return (NULL);
+		}
+		dog1->name = strcpy(copy_of_name, name);
+	}
+	else
+		dog1->name = NULL;
+
+	if (owner != NULL)
+	{
+		copy_of_owner = malloc(len(owner) + 1);
+		if (copy_of_owner == NULL)
+		{
+			free(copy_of_name);
+			free(dog1);
+			return (NULL);
+		}
+		dog1->owner = strcpy(copy_of_owner, owner);
+	}
+	else
+		dog1->owner = NULL;
+	return (dog1);
 }
